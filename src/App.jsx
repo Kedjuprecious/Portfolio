338cffd671a0dbb5944834c255d30a2af2731354
Kedjuprecious <kedjuprecious@gmail.com>
2023-05-26 15:47:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./index.css";
+import React from "react";
+import Header from "./components/Header/header";
+import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
+import Services from "./components/Services/Services";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Testimonial from "./components/Testimonial/Testimonial";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <Navbar /> */}
+      {/* <About /> */}
+      {/* <Experience /> */}
+      {/* <Services /> */}
+      {/* <Portfolio /> */}
+      {/* <Testimonial /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default App; // always export the component as default
